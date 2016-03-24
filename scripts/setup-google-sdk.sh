b954d83d168a7a8a21684982e630e2bc6b48d834
Brian Rosner <brosner@eldarion.com>
2016-03-24 16:33:39 -0600

    Improved Google SDK caching

--- scripts/setup-google-sdk.sh
+++ scripts/setup-google-sdk.sh
@@ -3,12 +3,12 @@ set -ev
 
 pip install pyopenssl
 
-if [ ! -d "${HOME}/google-cloud-sdk" ]; then
+if [ ! -f "${HOME}/google-cloud-sdk/.installed" ]; then
     curl -s https://sdk.cloud.google.com | bash
 else
     echo "Google Cloud SDK already installed; skipping"
 fi
+touch "${HOME}/google-cloud-sdk/.installed"
 
 openssl aes-256-cbc -K $encrypted_8a0dc7e65d70_key -iv $encrypted_8a0dc7e65d70_iv -in gcp-travisci.json.enc -out gcp-travisci.json -d
-
 gcloud auth activate-service-account --key-file gcp-travisci.json travisci@project-kel.iam.gserviceaccount.com
