2d9b49b59437d41b712ba82cb06f6474f0c7cff4
Brian Rosner <brosner@eldarion.com>
2016-04-01 16:50:09 -0600

    Install glide and build correct path

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,6 @@ set -ev
 
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_env.sh
+. $here/_gopath.sh
 
-CGO_ENABLED=0 go build $GO_BUILD_FLAGS -installsuffix cgo -ldflags "-s" -o bin/rproc
+CGO_ENABLED=0 go build $GO_BUILD_FLAGS -installsuffix cgo -ldflags "-s" -o bin/rproc $REPO_PATH
--- scripts/deps.sh
+++ scripts/deps.sh
@@ -7,4 +7,8 @@ here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 
 setup_gopath
 
+# install glide
+wget https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz
+tar -vxz -C $HOME/bin --strip=1 -f glide-0.10.1-linux-amd64.tar.gz
+
 glide install
