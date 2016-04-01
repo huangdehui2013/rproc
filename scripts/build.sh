59574b13688b0874a50191d90ebb90a07cd5a3de
Brian Rosner <brosner@eldarion.com>
2016-04-01 17:01:59 -0600

    Improved output in build and deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -ev
+set -e
 
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_env.sh
 . $here/_gopath.sh
 
-CGO_ENABLED=0 go build $GO_BUILD_FLAGS -installsuffix cgo -ldflags "-s" -o bin/rproc $REPO_PATH
+echo -n "Building rproc... "
+CGO_ENABLED=0 go build $GO_BUILD_FLAGS -installsuffix cgo -ldflags "-s" -o bin/rproc $REPO_PATH && echo "done"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -ev
+set -e
 
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_env.sh
 
-gsutil cp bin/rproc gs://release.kelproject.com/binaries/rproc/rproc-linux-amd64-$BUILD_TAG
+echo -n "Uploading rproc $BUILD_TAG... "
+gsutil cp bin/rproc gs://release.kelproject.com/binaries/rproc/rproc-linux-amd64-$BUILD_TAG && echo "done"
