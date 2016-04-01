991a30b8b64ed1d02d070be103a82c3e9f6c9c6e
Brian Rosner <brosner@eldarion.com>
2016-04-01 15:42:51 -0600

    Updated build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [ -n "$TRAVIS_TAG" ]; then
-    BUILD_TAG="$TRAVIS_TAG"
-else
-    BUILD_TAG="git-${TRAVIS_COMMIT:0:8}"
-fi
+here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
+. $here/_env.sh
 
 docker run -i --volume "$(pwd):/src" quay.io/kelproject/gb-build
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ex
 
-if [ -n "$TRAVIS_TAG" ]; then
-    BUILD_TAG="$TRAVIS_TAG"
-else
-    BUILD_TAG="git-${TRAVIS_COMMIT:0:8}"
-fi
+here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
+. $here/_env.sh
 
 gsutil cp bin/rproc gs://release.kelproject.com/binaries/rproc/rproc-linux-amd64-$BUILD_TAG
