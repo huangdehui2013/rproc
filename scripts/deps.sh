64974a672ffae12baa2b448672bb12b2226b1902
Brian Rosner <brosner@eldarion.com>
2016-04-01 16:54:02 -0600

    Fixed glide installation

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -8,7 +8,9 @@ here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 setup_gopath
 
 # install glide
+mkdir -p $HOME/bin
 wget https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz
 tar -vxz -C $HOME/bin --strip=1 -f glide-0.10.1-linux-amd64.tar.gz
+export PATH="$HOME/bin:${PATH}"
 
 glide install
