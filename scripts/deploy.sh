7d7020132552fb9b4f5ca963853ad3f5c71896be
Brian Rosner <brosner@eldarion.com>
2016-04-01 17:03:24 -0600

    Quiet gsutil

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_env.sh
 
 echo -n "Uploading rproc $BUILD_TAG... "
-gsutil cp bin/rproc gs://release.kelproject.com/binaries/rproc/rproc-linux-amd64-$BUILD_TAG && echo "done"
+gsutil -q cp bin/rproc gs://release.kelproject.com/binaries/rproc/rproc-linux-amd64-$BUILD_TAG && echo "done"
