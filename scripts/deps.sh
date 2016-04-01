c25dc6fa73238b536eb6f79144d48595c9d7be66
Brian Rosner <brosner@eldarion.com>
2016-04-01 17:08:39 -0600

    Better deps output

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 
 here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 . $here/_env.sh
@@ -7,10 +7,13 @@ here=$(cd "$(dirname "${BASH_SOURCE}")"; pwd -P)
 
 setup_gopath
 
-# install glide
-mkdir -p $HOME/bin
-wget https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz
-tar -vxz -C $HOME/bin --strip=1 -f glide-0.10.1-linux-amd64.tar.gz
+echo -n "Installing glide... "
+(
+    mkdir -p $HOME/bin
+    wget -q https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz
+    tar -xz -C $HOME/bin --strip=1 -f glide-0.10.1-linux-amd64.tar.gz
+) && echo "done"
 export PATH="$HOME/bin:${PATH}"
 
+echo "Installing glide dependencies..."
 glide install
